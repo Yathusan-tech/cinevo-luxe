b3e5ead2a4e34e5652c353272090e7bfed5ccf19
Yathusan-tech <yathusan795@gmail.com>
2026-09-12 00:35:49 +0530

    Remove unused legacy admin files

--- static/js/admin_movies.js
+++
@@ -1,7 +0,0 @@
-function confirmDelete(movieName) {
-    return confirm(
-        "Are you sure you want to delete \"" +
-        movieName +
-        "\"?\n\nThis action cannot be undone."
-    );
-}
